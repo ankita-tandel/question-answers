7541c14ef7069590f9d6f59cb454cfa2ba26ea9d
Ankita <ankita.tandel@weboniselab.com>
2015-06-22 15:37:27 +0530

    destroy answers on deleting its question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ActiveRecord::Base
-  has_many :answers
+  has_many :answers, dependent: :destroy
   accepts_nested_attributes_for :answers, :reject_if => lambda { |a| a[:description].blank? }, :allow_destroy => true
 
   validates :title, :description, presence: :true
